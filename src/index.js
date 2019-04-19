2c92fff12b76bc6d4384bc5618509bd0a0994f36
sgabenza <sgabenza86@gmail.com>
2019-04-19 15:34:46 +0200

    Acca Versione UNO v1

--- src/index.js
+++ src/index.js
@@ -1,23 +1,62 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import { App } from './components/App'
-import 'modern-normalize'
-import '@accurat/tachyons-lite'
-import 'tachyons-extra'
-import './reset.css'
-import './style.css'
+const t = document.createTextNode('CIAO')
 
-function renderApp() {
-  ReactDOM.render(<App />, document.getElementById('root'))
-}
+const a = document.createElement('span')
+a.setAttribute('class', 'wer')
+a.style.color = 'blue'
+a.appendChild(t)
 
-// First render
-renderApp()
+const e = document.createElement('div')
+e.setAttribute('class', 'asd')
+e.style.height = '50px'
+e.style.background = 'steelblue'
+e.appendChild(a)
 
-// Hot module reloading
-if (module.hot) {
-  module.hot.accept('components/App', () => {
-    console.clear()
-    renderApp()
+document.body.appendChild(e)
+
+// /////////////////////////////////////////////////////////////////////////////////////
+
+// prettier-ignore
+// const b = h('div', { class: 'asd', style: 'height: 50px; background: steelblue;' }, [
+const b = h('div', { class: 'asd', style: { height: '50px', background: 'steelblue' } }, [
+  h('span', { class: 'wer', style: 'color: blue;' }, [
+    'CIAO',
+  ]),
+])
+
+document.body.appendChild(b)
+
+// /////////////////////////////////////////////////////////////////////////////////////
+
+console.assert(a.isEqualNode(b), 'DIFFERENT!')
+
+/*
+1. v1
+2. parsing classes and id in tagName
+3. children destructuring and array of array
+4. umbrella/hiccup syntax
+*/
+
+// /////////////////////////////////////////////////////////////////////////////////////
+
+function h(tagName, attributes = {}, children = []) {
+  const element = document.createElement(tagName || 'div')
+  const keys = Object.keys(attributes)
+
+  keys.forEach(key => {
+    if (typeof attributes[key] === 'string') {
+      element.setAttribute(key, attributes[key])
+    } else {
+      element.setAttribute(
+        key,
+        JSON.stringify(attributes[key])
+          .replace(/\}|,/g, ';')
+          .replace(/\{|"/g, '')
+      )
+    }
   })
+  children.forEach(child =>
+    element.appendChild(typeof child === 'string' ? document.createTextNode(child) : child)
+  )
+
+  return element
 }
