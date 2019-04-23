7ec40c03ff438221b0d461007e5a7d6878bf2065
sgabenza <sgabenza86@gmail.com>
2019-04-23 09:58:29 +0200

    stash apply parsing

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ a.style.color = 'blue'
 a.appendChild(t)
 
 const e = document.createElement('div')
-e.setAttribute('class', 'asd')
+e.setAttribute('class', 'asd qwerty')
 e.style.height = '50px'
 e.style.background = 'steelblue'
 e.appendChild(a)
@@ -17,7 +17,7 @@ document.body.appendChild(e)
 
 // prettier-ignore
 // const b = h('div', { class: 'asd', style: 'height: 50px; background: steelblue;' }, [
-const b = h('div', { class: 'asd', style: { height: '50px', background: 'steelblue' } }, [
+const b = h('div.asd.qwerty', { style: { height: '50px', background: 'steelblue' } }, [
   h('span', { class: 'wer', style: 'color: blue;' }, [
     'CIAO',
   ]),
@@ -51,11 +51,38 @@ function isString(value) {
 }
 
 function h(tagName, attributes = {}, children = []) {
-  const element = document.createElement(tagName || 'div')
+  const tagNameSet = tagName.split(/(#\w+)|(\.\w+)/g)
+  const element = document.createElement(tagNameSet[0] || 'div')
+
+  tagNameSet.splice(0, 1)
+  let id
+  let classes = ''
+
+  tagNameSet.forEach(d => {
+    if (d) {
+      switch (d.charAt(0)) {
+        case '.':
+          classes = `${classes} ${d.replace(d.charAt(0), '')}`
+          break
+        case '#':
+          id = d.replace(d.charAt(0), '')
+          break
+      }
+    }
+  })
+
+  if (id) element.setAttribute('id', id)
+  if (classes) element.setAttribute('class', classes.trim())
 
   Object.entries(attributes).forEach(([key, value]) => {
     const attributeValue = isString(value) ? value : objectToStyleDeclaration(value)
-    element.setAttribute(key, attributeValue)
+
+    element.setAttribute(
+      key,
+      JSON.stringify(attributeValue)
+        .replace(/\}|,/g, ';')
+        .replace(/\{|"/g, '')
+    )
   })
 
   children.forEach(child =>
