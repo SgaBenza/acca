c1d2690f44a292c022632888f253f27a90361504
sgabenza <sgabenza86@gmail.com>
2019-05-03 16:55:37 +0200

    ♻️ refactor ✅

--- src/index.js
+++ src/index.js
@@ -1,24 +1,9 @@
-const t = document.createTextNode('CIAO')
-
-const a = document.createElement('span')
-a.setAttribute('class', 'wer')
-a.style.color = 'blue'
-a.appendChild(t)
-
-const e = document.createElement('div')
-e.setAttribute('class', 'asd qwerty')
-e.style.height = '50px'
-e.style.background = 'steelblue'
-e.appendChild(a)
-
-// document.body.appendChild(e)
-
-// /////////////////////////////////////////////////////////////////////////////////////
+import { h } from './lib/acca'
 
 let root = document.getElementById('root')
 
-const update = () => event => {
-  const { node, helloNode } = view(event.target.value)
+const update = event => {
+  const { helloNode } = view(event.target.value)
   const viewNode = root.childNodes[0]
 
   viewNode.removeChild(viewNode.childNodes[2])
@@ -31,7 +16,7 @@ function view(name) {
   node = h('div#view', {}, [
     h('input', {
       props: { type: 'text', placeholder: 'Type a your name' },
-      on: { input: update() },
+      on: { input: update },
     }),
     h('hr'),
     helloNode,
@@ -40,78 +25,3 @@ function view(name) {
 }
 
 root.appendChild(view('').node)
-
-// /////////////////////////////////////////////////////////////////////////////////////
-
-/*
-1. v1
-2. parsing classes and id in tagName
-3. children destructuring and array of array
-4. umbrella/hiccup syntax
-*/
-
-// /////////////////////////////////////////////////////////////////////////////////////
-
-function objectToStyleDeclaration(objectValue) {
-  return JSON.stringify(objectValue)
-    .replace(/\}|,/g, '; ')
-    .replace(/\{|"/g, '')
-    .replace(/:/g, ': ')
-    .trim()
-}
-
-function isString(value) {
-  return typeof value === 'string'
-}
-
-// prettier-ignore
-// const b = h('div', { class: 'asd', style: 'height: 50px; background: steelblue;' }, [
-function h(tagName, attributes = {}, children = []) {
-  const tagNameSet = tagName.split(/(#\w+)|(\.\w+)/g)
-  const element = document.createElement(tagNameSet[0] || 'div')
-
-  tagNameSet.splice(0, 1)
-  let id
-  let classes = ''
-
-  tagNameSet.forEach(d => {
-    if (d) {
-      switch (d.charAt(0)) {
-        case '.':
-          classes = `${classes} ${d.replace(d.charAt(0), '')}`
-          break
-        case '#':
-          id = d.replace(d.charAt(0), '')
-          break
-      }
-    }
-  })
-
-  if (id) element.setAttribute('id', id)
-  if (classes) element.setAttribute('class', classes.trim())
-
-  if (attributes.props) {
-    Object.entries(attributes.props).forEach(([key, value]) => {
-      const attributeValue = isString(value) ? value : objectToStyleDeclaration(value)
-
-      element.setAttribute(
-        key,
-        JSON.stringify(attributeValue)
-          .replace(/\}|,/g, ';')
-          .replace(/\{|"/g, '')
-      )
-    })
-  }
-
-  if (attributes.on) {
-    Object.entries(attributes.on).forEach(([event, action]) => {
-      element.addEventListener(event, action)
-    })
-  }
-
-  children.forEach(child =>
-    element.appendChild(isString(child) ? document.createTextNode(child) : child)
-  )
-
-  return element
-}
---
+++ src/lib/acca.js
@@ -0,0 +1,61 @@
+function objectToStyleDeclaration(objectValue) {
+  return JSON.stringify(objectValue)
+    .replace(/\}|,/g, '; ')
+    .replace(/\{|"/g, '')
+    .replace(/:/g, ': ')
+    .trim()
+}
+
+function isString(value) {
+  return typeof value === 'string'
+}
+
+export function h(tagName, attributes = {}, children = []) {
+  const tagNameSet = tagName.split(/(#\w+)|(\.\w+)/g)
+  const element = document.createElement(tagNameSet[0] || 'div')
+
+  tagNameSet.splice(0, 1)
+  let id
+  let classes = ''
+
+  tagNameSet.forEach(d => {
+    if (d) {
+      switch (d.charAt(0)) {
+        case '.':
+          classes = `${classes} ${d.replace(d.charAt(0), '')}`
+          break
+        case '#':
+          id = d.replace(d.charAt(0), '')
+          break
+      }
+    }
+  })
+
+  if (id) element.setAttribute('id', id)
+  if (classes) element.setAttribute('class', classes.trim())
+
+  if (attributes.props) {
+    Object.entries(attributes.props).forEach(([key, value]) => {
+      const attributeValue = isString(value) ? value : objectToStyleDeclaration(value)
+
+      element.setAttribute(
+        key,
+        JSON.stringify(attributeValue)
+          .replace(/\}|,/g, ';')
+          .replace(/\{|"/g, '')
+      )
+    })
+  }
+
+  if (attributes.on) {
+    Object.entries(attributes.on).forEach(([event, action]) => {
+      element.addEventListener(event, action)
+    })
+  }
+
+  children.forEach(child =>
+    element.appendChild(isString(child) ? document.createTextNode(child) : child)
+  )
+
+  return element
+}
