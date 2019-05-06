e73a7c78a41b7fe6632e471f6ba44a0a67094f4e
sgabenza <sgabenza86@gmail.com>
2019-05-06 18:36:15 +0200

    🤯 keep focused and set caret position at the end

--- src/index.js
+++ src/index.js
@@ -1,36 +1,41 @@
 import { h } from './lib/acca'
-import { state } from './lib/state'
 import { removeChildren } from './lib/vdom'
 
 const root = document.getElementById('root')
 
 const update = event => {
-  state.text += event.target.value
-  state.isFocused = true
+  const state = { text: event.target.value, isFocused: true }
+
   removeChildren(root)
-  render()
+  render(state)
+
+  const input = document.getElementById('input')
+  const pos = input.value.length
+
+  // keep focused and set caret position at the end
+  input.focus()
+  input.setSelectionRange(pos, pos)
 }
 
-function view(name) {
+function view(state) {
   let node = h('div#view', {}, [
-    h('input', {
+    h('input#input', {
       props: {
         type: 'text',
         placeholder: 'Type a your name',
-        autofocus: state.isFocused,
         value: state.text,
       },
       on: { input: update },
     }),
     h('hr'),
-    h('div#patch', {}, [`Hello ${name}`]),
+    h('div#patch', {}, [`Hello ${state.text}`]),
   ])
 
   return node
 }
 
-function render() {
-  root.appendChild(view(state.text))
+function render(state) {
+  root.appendChild(view(state))
 }
 
-render()
+render({ text: '', isFocused: false })
