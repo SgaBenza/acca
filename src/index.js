07f501e385fcde6875e257bd78e748e18334174a
sgabenza <sgabenza86@gmail.com>
2019-05-07 10:08:06 +0200

    refactor

--- src/index.js
+++ src/index.js
@@ -2,40 +2,42 @@ import { h } from './lib/acca'
 import { removeChildren } from './lib/vdom'
 
 const root = document.getElementById('root')
+const state = { text: '', isFocused: false }
 
 const update = event => {
-  const state = { text: event.target.value, isFocused: true }
+  state.text = event.target.value
+  state.isFocused = true
 
   removeChildren(root)
   render(state)
 
+  // keep focused and set caret position at the end
   const input = document.getElementById('input')
   const pos = input.value.length
 
-  // keep focused and set caret position at the end
-  input.focus()
+  state.isFocused && input.focus()
   input.setSelectionRange(pos, pos)
 }
 
-function view(state) {
+function view(actualState) {
   let node = h('div#view', {}, [
     h('input#input', {
       props: {
         type: 'text',
         placeholder: 'Type a your name',
-        value: state.text,
+        value: actualState.text,
       },
       on: { input: update },
     }),
     h('hr'),
-    h('div#patch', {}, [`Hello ${state.text}`]),
+    h('div#patch', {}, [`Hello ${actualState.text}`]),
   ])
 
   return node
 }
 
-function render(state) {
-  root.appendChild(view(state))
+function render(actualState) {
+  root.appendChild(view(actualState))
 }
 
 render({ text: '', isFocused: false })
