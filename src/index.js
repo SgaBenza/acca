e0c08e845cd064b51147057cfa39e7cb07d7d59f
sgabenza <sgabenza86@gmail.com>
2019-07-11 10:09:44 +0200

    parseRequestURL() check actual pathname

--- src/index.js
+++ src/index.js
@@ -1,32 +1,30 @@
 import { render, h } from './lib/acca'
-import { Router } from './lib/router'
+import { Router, parseRequestURL } from './lib/router'
 import { routes } from './routes'
 import { navigation } from './components/pages/navigation'
 import { textInputView } from './components/pages/text-input'
 import './style.css'
 
 const textInputState = { textUno: 'text', textDue: 't2t' }
-// const routerState = { pathname: parseRequestURL() }
-const routerState = { pathname: '/' }
+const routerState = { pathname: parseRequestURL() }
 
 const view = () =>
   h('div', {}, [Router({ routes, pathname: routerState.pathname, state: textInputState })])
 
+function handlePathname(pathname) {
+  routerState.pathname = pathname || parseRequestURL()
+}
+
 render(view)
 
+// listeners URL changes and pages loaded
+window.addEventListener('popstate', () => {
+  console.log(location.pathname)
+  // handlePathnameChange(location.pathname)
+})
+
 // const view = () => routes(textInputState)['/']
-
-/* function handlePathname(pathname) {
-  routerState.pathname = pathname || parseRequestURL()
-
-  render(view)
-} */
 
 /* function handlePathnameChange(pathname) {
   routerState.pathname = pathname
 } */
-
-// listeners URL changes and pages loaded
-/* window.addEventListener('popstate', () => {
-  handlePathnameChange(location.pathname)
-}) */
